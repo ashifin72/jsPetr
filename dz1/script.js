536014ce50c441acad7d5309288bb5e7f246164b
Ashifin <ashfedor@gmail.com>
2020-11-16 16:47:36 +0200

    доработка 1 ДЗ

--- dz1/script.js
+++ dz1/script.js
@@ -1,16 +1,21 @@
 'use strict';
 
 let money = +prompt("Ваш бюджет на месяц?", ''),
-  time = prompt("Введите дату в формате YYYY-MM-DD", ''),
-  question1 = prompt('Введите обязательную статью расходов в этом месяце', ""),
-  question2 = prompt('Во сколько обойдется?', "");
+  time = prompt("Введите дату в формате YYYY-MM-DD", '');
+
 
 let appData = {
   budget: money,
   timeData: time,
-  expenses: question1 + " " + question2
+  expenses: {},
+  optionalExpenses: {},
+  income: [],
+  savings: false
 };
+let quest1 = prompt('Введите обязательную статью расходов в этом месяце', ''),
+  quest2 = prompt('Во сколько обойдется?', '');
+
 console.log(appData.budget);
 console.log(appData.timeData);
-console.log(appData.expenses);
-alert(appData.budget/30);
+console.log(appData.expenses.quest1 = quest2);
+alert(appData.budget / 30);
--- js/common.js
+++ js/common.js
@@ -3,3 +3,8 @@ if (2*4 == 6) {
 }else{
   console.log('Неверно!')
 }
+
+// let x = 5; alert( x++ );
+let y = 1; let x = [ ] + 1 + 2; alert(0 || "" || 2 || undefined || true || falsе);
+
+console.log([ ] + false - null + true );
